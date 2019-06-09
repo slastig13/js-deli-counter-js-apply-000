8d64e252fc3a1ea17e18a71418baea691e4ba6b8
Samantha Lastig <slastig13@gmail.com>
2019-06-09 15:13:29 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,3 +1,12 @@
-function takeANumber(katzDeliLine, name){
-  return `Welcome, ${name}. You are number ${katzDeliLine.length + 1} in line.`;
+// function takeANumber(katzDeliLine, name){
+//   return `Welcome, ${name}. You are number ${katzDeliLine.length + 1} in line.`;
+// }
+
+function nowServing(katzDeliLine) {
+  if (katzDeliLine.length === 0) {
+    return "There is nobody waiting to be served!";
+  } else {
+  let firstPerson = katzDeliLine.shift();
+  return firstPerson;
+  }
 }
